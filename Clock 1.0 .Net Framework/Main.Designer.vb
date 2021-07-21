ae53383184fbdf047e011f0b53acf70082fa9bb4
WindowsSystem32 <wincom811@naver.com>
2021-07-21 18:11:56 +0900

    1.0

---
+++ Clock 1.0 .Net Framework/Main.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.pb = New System.Windows.Forms.PictureBox()
+        Me.tmr1 = New System.Windows.Forms.Timer(Me.components)
+        Me.btn_about = New System.Windows.Forms.Button()
+        CType(Me.pb, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pb
+        '
+        Me.pb.BackColor = System.Drawing.Color.Black
+        Me.pb.Location = New System.Drawing.Point(0, 0)
+        Me.pb.Name = "pb"
+        Me.pb.Size = New System.Drawing.Size(800, 426)
+        Me.pb.TabIndex = 0
+        Me.pb.TabStop = False
+        '
+        'tmr1
+        '
+        Me.tmr1.Interval = 10
+        '
+        'btn_about
+        '
+        Me.btn_about.BackColor = System.Drawing.Color.Black
+        Me.btn_about.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_about.ForeColor = System.Drawing.Color.Lime
+        Me.btn_about.Location = New System.Drawing.Point(0, 426)
+        Me.btn_about.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btn_about.Name = "btn_about"
+        Me.btn_about.Size = New System.Drawing.Size(75, 23)
+        Me.btn_about.TabIndex = 1
+        Me.btn_about.Text = "About"
+        Me.btn_about.UseVisualStyleBackColor = False
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(800, 449)
+        Me.Controls.Add(Me.btn_about)
+        Me.Controls.Add(Me.pb)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.MinimumSize = New System.Drawing.Size(688, 246)
+        Me.Name = "Main"
+        Me.Text = "Clock 1.0 (.NET Framework 4.8) by WindowsSystem32"
+        CType(Me.pb, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pb As PictureBox
+    Friend WithEvents tmr1 As Timer
+    Friend WithEvents btn_about As Button
+End Class
---
+++ Clock 1.0 .Net Framework/Main.vb
@@ -0,0 +1,223 @@
+﻿Imports System.ComponentModel
+
+Public Class Main
+    Dim dc As Color = Color.Lime
+    Dim lev = 5
+    Dim num As Integer() = New Integer(5) {0, 0, 0, 0, 0, 0}
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        setTime(2)
+        tmr1.Start()
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        tmr1.Stop()
+    End Sub
+
+    Private Sub tmr1_Tick(sender As Object, e As EventArgs) Handles tmr1.Tick
+        If lev >= 5 Then
+            setTime(1)
+            Do Until Not (num(2) = num(5))
+                setTime(0)
+            Loop
+        End If
+        Dim ms As Integer = DateTime.Now.Millisecond
+        If ms <= 75 Then
+            lev = 0
+        ElseIf ms <= 150 Then
+            lev = 1
+        ElseIf ms <= 225 Then
+            lev = 2
+        ElseIf ms <= 300 Then
+            lev = 3
+        ElseIf ms <= 375 Then
+            lev = 4
+        Else
+            lev = 5
+        End If
+        clear()
+        Dim h1 As Integer
+        Dim h2 As Integer
+        Dim h3 As Integer
+        Dim h4 As Integer
+        Dim m1 As Integer
+        Dim m2 As Integer
+        Dim m3 As Integer
+        Dim m4 As Integer
+        Dim s1 As Integer
+        Dim s2 As Integer
+        Dim s3 As Integer
+        Dim s4 As Integer
+        h2 = num(3) Mod 10
+        h1 = (num(3) - h2) / 10
+        h4 = num(0) Mod 10
+        h3 = (num(0) - h4) / 10
+        m2 = num(4) Mod 10
+        m1 = (num(4) - m2) / 10
+        m4 = num(1) Mod 10
+        m3 = (num(1) - m4) / 10
+        s2 = num(5) Mod 10
+        s1 = (num(5) - s2) / 10
+        s4 = num(2) Mod 10
+        s3 = (num(2) - s4) / 10
+        seg(New Point(-292, 0), h3, h1, lev)
+        seg(New Point(-188, 0), h4, h2, lev)
+        If num(5) Mod 2 = 0 Then
+            fillEllip(dc, -120, -36, 16, 16, 100)
+            fillEllip(dc, -120, 36, 16, 16, 100)
+        End If
+        seg(New Point(-52, 0), m3, m1, lev)
+        seg(New Point(52, 0), m4, m2, lev)
+        If num(5) Mod 2 = 0 Then
+            fillEllip(dc, 120, -36, 16, 16, 100)
+            fillEllip(dc, 120, 36, 16, 16, 100)
+        End If
+        seg(New Point(188, 0), s3, s1, lev)
+        seg(New Point(292, 0), s4, s2, lev)
+    End Sub
+
+    Private Sub fillRect(col As Color, x As Integer, y As Integer, wid As Integer, hei As Integer, n1 As Integer)
+        Dim wid2 As Integer = wid * (n1 / 100)
+        Dim hei2 As Integer = hei * (n1 / 100)
+        Dim half_wid2 As Integer = wid2 / 2
+        Dim half_hei2 As Integer = hei2 / 2
+        Dim half_pbwid As Integer = pb.Size.Width / 2
+        Dim half_pbhei As Integer = pb.Size.Height / 2
+        pb.CreateGraphics.FillRectangle(New SolidBrush(col), half_pbwid - half_wid2 + x, half_pbhei - half_hei2 + y, wid2, hei2)
+    End Sub
+
+    Private Sub fillEllip(col As Color, x As Integer, y As Integer, wid As Integer, hei As Integer, sz As Integer)
+        Dim wid2 As Integer = wid * (sz / 100)
+        Dim hei2 As Integer = hei * (sz / 100)
+        Dim half_wid2 As Integer = wid2 / 2
+        Dim half_hei2 As Integer = hei2 / 2
+        Dim half_pbwid As Integer = pb.Size.Width / 2
+        Dim half_pbhei As Integer = pb.Size.Height / 2
+        pb.CreateGraphics.FillEllipse(New SolidBrush(col), half_pbwid - half_wid2 + x, half_pbhei - half_hei2 + y, wid2, hei2)
+    End Sub
+
+    Private Sub clear()
+        pb.CreateGraphics.Clear(pb.BackColor)
+    End Sub
+
+    Private Sub seg(loc As Point, inf1 As Integer, inf2 As Integer, lev As Integer) 'wid: 88, hei: 184'
+        '_A_'
+        'B_C'
+        '_D_'
+        'E_F'
+        '_G_'
+        Dim sx As Integer = loc.X
+        Dim sy As Integer = loc.Y
+        Dim sz1 As Integer()
+        Dim sz2 As Integer()
+        Dim sz3 As Integer()
+        sz1 = segc1(inf1)
+        sz2 = segc1(inf2)
+        sz3 = segc2(sz1, sz2, lev)
+        'sz3 = sz1'
+        fillRect(dc, 0 + sx, -72 + sy + sz3(7), 56, 16, sz3(0)) 'A'
+        fillRect(dc, -36 + sx + sz3(8), -36 + sy, 16, 56, sz3(1)) 'B'
+        fillRect(dc, 36 + sx + sz3(9), -36 + sy, 16, 56, sz3(2)) 'C'
+        fillRect(dc, 0 + sx, 0 + sy, 56, 16, sz3(3)) 'D'
+        fillRect(dc, -36 + sx + sz3(10), 36 + sy, 16, 56, sz3(4)) 'E'
+        fillRect(dc, 36 + sx + sz3(11), 36 + sy, 16, 56, sz3(5)) 'F'
+        fillRect(dc, 0 + sx, 72 + sy + sz3(12), 56, 16, sz3(6)) 'G'
+    End Sub
+
+    Private Function segc1(input As Integer)
+        Dim num1 As Integer = 25
+        Dim num2 As Integer = 100
+        Dim output As Integer() = New Integer(12) {num2, num2, num2, num2, num2, num2, num2, 0, 0, 0, 0, 0, 0} 'wid1: 56, hei1: 16 | wid2: 14, hei2: 4'
+        If input = 0 Or input = 2 Or input = 3 Or input = 5 Or input = 6 Or input = 7 Or input = 8 Or input = 9 Then 'A'
+            output(0) = num2
+        Else
+            output(0) = num1
+            output(7) = 25
+        End If
+        If input = 0 Or input = 4 Or input = 5 Or input = 6 Or input = 8 Or input = 9 Then 'B'
+            output(1) = num2
+        Else
+            output(1) = num1
+            output(8) = 25
+        End If
+        If input = 0 Or input = 1 Or input = 2 Or input = 3 Or input = 4 Or input = 7 Or input = 8 Or input = 9 Then 'C'
+            output(2) = num2
+        Else
+            output(2) = num1
+            output(9) = -25
+        End If
+        If input = 2 Or input = 3 Or input = 4 Or input = 5 Or input = 6 Or input = 8 Or input = 9 Then 'D'
+            output(3) = num2
+        Else
+            output(3) = num1
+        End If
+        If input = 2 Or input = 6 Or input = 8 Or input = 0 Then 'E'
+            output(4) = num2
+        Else
+            output(4) = num1
+            output(10) = 25
+        End If
+        If input = 0 Or input = 1 Or input = 3 Or input = 4 Or input = 5 Or input = 6 Or input = 7 Or input = 8 Or input = 9 Then 'F'
+            output(5) = num2
+        Else
+            output(5) = num1
+            output(11) = -25
+        End If
+        If input = 0 Or input = 2 Or input = 3 Or input = 5 Or input = 6 Or input = 8 Or input = 9 Then 'G'
+            output(6) = num2
+        Else
+            output(6) = num1
+            output(12) = -25
+        End If
+        Return output
+    End Function
+
+    Private Function segc2(input1() As Integer, input2() As Integer, lev As Integer)
+        Dim output As Integer() = New Integer(12) {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} 'wid1: 56, hei1: 16 | wid2: 14, hei2: 4'
+        For i = 0 To 6
+            If input1(i) = input2(i) Then
+                output(i) = input1(i)
+            Else
+                output(i) = ((input2(i) - input1(i)) / 5) * lev + input1(i)
+            End If
+        Next
+        For i = 7 To 12
+            If input1(i) = input2(i) Then
+                output(i) = input1(i)
+            Else
+                output(i) = ((input2(i) - input1(i)) / 5) * lev + input1(i)
+            End If
+        Next
+        Return output
+    End Function
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        pb.Size = New Size(Size.Width - 16, Size.Height - 87)
+        btn_about.Location = New Point(0, Size.Height - 62)
+    End Sub
+
+    Private Sub setTime(mode As Integer)
+        Dim h As Integer = DateTime.Now.Hour
+        Dim m As Integer = DateTime.Now.Minute
+        Dim s As Integer = DateTime.Now.Second
+        If mode = 0 Then
+            num(3) = h
+            num(4) = m
+            num(5) = s
+        ElseIf mode = 1 Then
+            num(0) = h
+            num(1) = m
+            num(2) = s
+        Else
+            num(0) = h
+            num(1) = m
+            num(2) = s
+            num(3) = h
+            num(4) = m
+            num(5) = s
+        End If
+    End Sub
+
+    Private Sub btn_about_Click(sender As Object, e As EventArgs) Handles btn_about.Click
+        About.ShowDialog()
+    End Sub
+End Class
